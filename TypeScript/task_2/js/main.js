39bce492c3f96e782b8b10f8be9345f731f9812e
vnporras <vnporras1028@gmail.com>
2024-10-01 01:39:39 -0500

    Creating functions specific to employees

---
+++ TypeScript/task_2/js/main.js
@@ -0,0 +1,56 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+console.log(createEmployee(200));
+//Teacher
+console.log(createEmployee(1000));
+//Director
+console.log(createEmployee('$500'));
+//Director
+// 6. Creating functions specific to employees
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else {
+        return employee.workTeacherTasks();
+    }
+}
+var employee1 = createEmployee(200);
+console.log(executeWork(employee1));
+var employee2 = createEmployee(1000);
+console.log(executeWork(employee2));
